8b03046a2d43aab7991a463c5c31c67d7a30827d
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-03-18 18:06:19 +0100

    starting pack

---
+++ app/src/main/java/com/example/todoapp_kotlin/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp_kotlin.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp_kotlin.R
+
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentlist = arrayListOf<Fragment>(
+
+        )
+        return view
+    }
+
+}
